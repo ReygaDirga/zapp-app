4e2daaeae3ddda5b1df9fb5f7d796c250692ba36
darrentimotius <timotiusdarren@gmail.com>
2026-02-23 11:09:00 +0700

    chore: increase API connect and receive timeout duration

--- lib/features/detail/apiclient.dart
+++ lib/features/detail/apiclient.dart
@@ -5,8 +5,8 @@ class ApiClient {
   static final Dio dio = Dio(
     BaseOptions(
       baseUrl: 'https://api.zapp.software',
-      connectTimeout: const Duration(seconds: 10),
-      receiveTimeout: const Duration(seconds: 10),
+      connectTimeout: const Duration(seconds: 30),
+      receiveTimeout: const Duration(seconds: 60),
       headers: {
         'Content-Type': 'application/json',
       },
